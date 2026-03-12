1d9beaec7dd2c66355dee4375ca05bed4a92853c
Jahanvi Singh <js3748@srmist.edu.in>
2026-03-12 10:43:42 +0530

    Added UseCase1 code

--- src/BookMyStayApp/BookMyStayApp.java
+++ src/BookMyStayApp/BookMyStayApp.java
@@ -1,4 +1,26 @@
 package BookMyStayApp;
 
+/**
+ * Book My Stay App
+ * Use Case 1: Application Entry & Welcome Message
+ *
+ * This class represents the entry point of the Hotel Booking application.
+ *
+ * @author Ragul
+ * @version 1.0
+ */
+
 public class BookMyStayApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("=====================================");
+        System.out.println("        Welcome to Book My Stay      ");
+        System.out.println("      Hotel Booking Management App   ");
+        System.out.println("             Version 1.0             ");
+        System.out.println("=====================================");
+
+        System.out.println("Application started successfully.");
+        System.out.println("Thank you for using Book My Stay!");
+    }
 }
